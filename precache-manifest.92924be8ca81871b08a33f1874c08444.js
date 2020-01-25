39f66133726b7bac95d0beb4983d01ea8d128684
Christopher Ruiz <chrisruizaz@gmail.com>
2020-01-25 14:16:34 -0700

    David's Dirty Hack

---
+++ precache-manifest.92924be8ca81871b08a33f1874c08444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c3e277ada0889342dbf7bc545dd94e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a911875ccb1df906e82",
+    "url": "/static/css/main.892fe04a.chunk.css"
+  },
+  {
+    "revision": "09e0c520aafc9c2592fe",
+    "url": "/static/js/2.a43fedb6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a43fedb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a911875ccb1df906e82",
+    "url": "/static/js/main.aa39f544.chunk.js"
+  },
+  {
+    "revision": "e2f430c476d095f95309",
+    "url": "/static/js/runtime-main.1e8ee8a5.js"
+  }
+]);
